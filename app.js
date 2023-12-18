e7d21d0c027afe0574362c1cba016742129675d9
Seth <urchen46@gmail.com>
2023-12-18 09:15:45 -0700

    create website

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3005;
+
+
+app.use(express.static('stuff'));
+
+
+app.get(/book/, (req, res) =>{
+    res.send('\ngot you\n');
+});
+
+app.route('/banana')
+    .get((req,res)=>{
+        res.send('get request\n')
+    })
+    .put((req,res)=>{
+        res.send('get put\n')
+    })
+    .delete((req,res)=>{
+        res.send('get delete\n')
+    })
+
+app.get(/.*apple$/, (req, res) =>{
+    res.send('\n you got a fruit \n')
+})
+
+app.listen(port, ()=> {
+    console.log(`on port: ${port}`);
+});
+
